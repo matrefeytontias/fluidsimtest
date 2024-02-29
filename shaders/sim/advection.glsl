54a42ba2873f51718d7c4b0bc98dc7a50830eea2
Mattias Refeyton <mrefeyton@microsoft.com>
2024-02-29 15:57:37 +0000

    Fixed monotonic cubic interpolation in advection shader + update thirdparty

--- shaders/sim/advection.glsl
+++ shaders/sim/advection.glsl
@@ -53,20 +53,24 @@ vec2 traceBack(vec2 position)
 
 // Visual Simulation of Smoke, Ronald Fedkiw, Jos Stam and Henrik Wann Jensen: Proceedings of SIGGRAPH'2001
 // Appendix B Monotonic Cubic Interpolation
+// and
+// https://jbrd.github.io/2020/12/27/monotone-cubic-interpolation.html
+// A More Efficient Monotone-Cubic Sampler
 float monotonicCubicInterpolation(float qprev, float q0, float q1, float qnext, float t)
 {
 	float delta = q1 - q0;
 	float d0 = (q1 - qprev) * 0.5;
 	float d1 = (qnext - q0) * 0.5;
 
-	// Enforce monotonicity
-	d0 = sign(delta) != sign(d0) ? 0. : d0;
-	d1 = sign(delta) != sign(d1) ? 0. : d1;
+	// Enforce monotonicity + clamp gradients
+	d0 = sign(delta) != sign(d0) ? 0. : d0 / delta > 3 ? delta * 3 : d0;
+	d1 = sign(delta) != sign(d1) ? 0. : d1 / delta > 3 ? delta * 3 : d1;
 
 	float a0 = q0;
 	float a1 = d0;
 	float a2 = delta * 3 - d0 * 2 - d1;
-	float a3 = d0 + d1 - delta;
+	// WARNING : the Stam paper forgets the 2 here
+	float a3 = d0 + d1 - delta * 2;
 
 	return ((a3 * t + a2) * t + a1) * t + a0;
 }
@@ -74,20 +78,27 @@ float monotonicCubicInterpolation(float qprev, float q0, float q1, float qnext, 
 // Monotonic bicubic interpolation
 float interpolateField(vec2 uv)
 {
-	vec2 texelOffset = uGridParams.oneOverGridSize;
+	vec2 size = textureSize(uFieldIn, 0).xy;
+
+	// Gather exactly in the corner of the texel so the correct texels are always fetched.
+	// Not doing this introduces irregularities on texel boundaries.
+	vec2 realTexelSample = uv * size - 0.5;
+	vec2 cornerTexelSample = floor(realTexelSample);
+	vec2 gatherUV = (cornerTexelSample + 1.) * uGridParams.oneOverGridSize;
 
 	// Interpolate along X then Y
-	vec4 topLeftBlock = textureGather(uFieldIn, texelOffset * vec2(-1, 1) + uv);
-	vec4 topRightBlock = textureGather(uFieldIn, texelOffset + uv);
-	vec4 bottomLeftBlock = textureGather(uFieldIn, -texelOffset + uv);
-	vec4 bottomRightBlock = textureGather(uFieldIn, texelOffset * vec2(1, -1) + uv);
+	vec4 topLeftBlock = textureGatherOffset(uFieldIn, gatherUV, ivec2(-1, 1));
+	vec4 topRightBlock = textureGatherOffset(uFieldIn, gatherUV, ivec2(1, 1));
+	vec4 bottomLeftBlock = textureGatherOffset(uFieldIn, gatherUV, ivec2(-1, -1));
+	vec4 bottomRightBlock = textureGatherOffset(uFieldIn, gatherUV, ivec2(1, -1));
 
-	vec2 t = fract(uv * textureSize(uFieldIn, 0).xy - 0.5);
-
-	float q0 = monotonicCubicInterpolation(topLeftBlock.x, topLeftBlock.y, topRightBlock.x, topRightBlock.y, t.x);
-	float q1 = monotonicCubicInterpolation(topLeftBlock.w, topLeftBlock.z, topRightBlock.w, topRightBlock.z, t.x);
-	float q2 = monotonicCubicInterpolation(bottomLeftBlock.x, bottomLeftBlock.y, bottomRightBlock.x, bottomRightBlock.y, t.x);
-	float q3 = monotonicCubicInterpolation(bottomLeftBlock.w, bottomLeftBlock.z, bottomRightBlock.w, bottomRightBlock.z, t.x);
+	vec2 t = realTexelSample - cornerTexelSample;
+	
+	// Y goes up
+	float q0 = monotonicCubicInterpolation(bottomLeftBlock.w, bottomLeftBlock.z, bottomRightBlock.w, bottomRightBlock.z, t.x);
+	float q1 = monotonicCubicInterpolation(bottomLeftBlock.x, bottomLeftBlock.y, bottomRightBlock.x, bottomRightBlock.y, t.x);
+	float q2 = monotonicCubicInterpolation(topLeftBlock.w, topLeftBlock.z, topRightBlock.w, topRightBlock.z, t.x);
+	float q3 = monotonicCubicInterpolation(topLeftBlock.x, topLeftBlock.y, topRightBlock.x, topRightBlock.y, t.x);
 
 	return monotonicCubicInterpolation(q0, q1, q2, q3, t.y);
 }
